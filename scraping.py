9992de690876ad3efd23f685bea1aac1a351318d
cgruns4 <71041680+cgruns4@users.noreply.github.com>
2021-04-25 18:24:03 -0400

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -18,14 +18,15 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
     browser.quit()
     return data
 
-    def mars_news(browser):
+def mars_news(browser):
 
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
@@ -58,7 +59,7 @@ def scrape_all():
     
     return news_title, news_p
 
-    def featured_image(browser):
+def featured_image(browser):
 
     # Visit URL
     url = 'https://spaceimages-mars.com'
@@ -106,6 +107,31 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/index.html'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    images = img_soup.find_all('img')
+    images
+    #img_url= img_soup.find('img', class_='fancybox-image').get('src')
+
+    for img in images:
+        img_url = img['src']
+        img_title = img['alt']
+        hemisphere_image_urls.append({"url": f"https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/{img_url}", "title":img_title})
+    
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
